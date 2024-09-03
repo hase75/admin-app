a22cd8c492dc3156befac4b2ad0da5c90f0c7b9c
Keigo <122374696+hase75@users.noreply.github.com>
2024-09-04 00:23:55 +0900

    ダッシュボード追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MembersComponent } from './members/members.component';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{ path: 'members', component: MembersComponent }]; //membersというパスにアクセスした際にMembersComponentを表示させる
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard' , pathMatch: 'full'}, //full:パスに完全一位しているか
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'members', component: MembersComponent} //membersというパスにアクセスした際にMembersComponentを表示させる
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)], //RouterModuleというroutingを行う機能をアプリケーション全体に適応する際にroutes情報を渡してroutingを有効にする処理
+  exports: [RouterModule], //app.module.tsでルーティングの処理を使えるようにする設定
 })
 export class AppRoutingModule {}
