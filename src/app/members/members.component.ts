fd28f08d9920c3245a616e00aae9cc415f9f071c
Keigo <122374696+hase75@users.noreply.github.com>
2024-09-03 22:25:53 +0900

    社員を選択した際のメッセージログ表示

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member} from '../member';
 import { MemberService } from '../member.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-members',
@@ -13,7 +14,10 @@ export class MembersComponent implements OnInit {
   selectedMember?: Member; // オプショナルプロパティとして定義
 
   //自身のプロパティなどのデータを初期化するために使われる
-  constructor(private memberService: MemberService) {} //DI（Dependency Injection）　依存していた部分を、外から注入すること
+  constructor(
+    private memberService: MemberService,
+    private messageService: MessageService
+    ) {} //DI（Dependency Injection）　依存していた部分を、外から注入すること
 
   //外部からデータを取得する際はngOnInit内に記述する
   //ライフサイクルメソッド
@@ -23,6 +27,7 @@ export class MembersComponent implements OnInit {
 
   onSelect(member: Member): void {
     this.selectedMember = member;
+    this.messageService.add(`MemberComponent: 社員データ(id=${member.id}が選択されました)`);
   }
 
   getMembers(): void {
