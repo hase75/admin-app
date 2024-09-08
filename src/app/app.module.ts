75fbb4d28fce2720d451b44710bb58ea95fd228e
Keigo <122374696+hase75@users.noreply.github.com>
2024-09-08 17:42:45 +0900

    HTTPクライアントでデータ取得

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
-import { MemberDetailComponent } from "./member-detail/member-detail.component";
-import { MessagesComponent } from "./messages/messages.component";
+import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -15,14 +18,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MembersComponent,
     MemberDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
-],
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -3,21 +3,45 @@ import { MEMBERS } from './mock-members';
 import { Observable, of } from 'rxjs'; //非同期処理
 import { Member } from './member';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root' //アプリケーションのどこからでも使用できる
 })
 export class MemberService {
+  private membersUrl = 'api/members'
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
 
   getMembers(): Observable<Member[]> {
-    this.messageService.add(`MemberService: 社員一覧データを取得しました`)
-    return of(MEMBERS); //ofでMEMBERSの値をObservableに変換している
+    return this.http.get<Member[]>(this.membersUrl)
+      .pipe(
+        tap(members => this.log('社員データを取得しました')),
+        catchError(this.handleError<Member[]>('getMembers', []))
+      );
   }
 
   getMember(id: number): Observable<Member> {
     this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
     return of(MEMBERS.find(member => member.id === id)!); //配列の中でidがマッチするものを1つだけ取得
+  }
+
+  private log(message: string) {
+    this.messageService.add(`MemberService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return(error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} 失敗: ${error.message}`);
+
+      return of(result as T);
+    }
   }
 }
