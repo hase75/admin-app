c47383167cea60562f429556aa758a0b148397f7
Keigo <122374696+hase75@users.noreply.github.com>
2024-09-09 23:20:09 +0900

    詳細データの取得と更新

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -3,7 +3,7 @@ import { MEMBERS } from './mock-members';
 import { Observable, of } from 'rxjs'; //非同期処理
 import { Member } from './member';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
 
@@ -11,7 +11,10 @@ import { catchError, map, tap } from 'rxjs/operators';
   providedIn: 'root' //アプリケーションのどこからでも使用できる
 })
 export class MemberService {
-  private membersUrl = 'api/members'
+  private membersUrl = 'api/members';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  }
 
   constructor(
     private http: HttpClient,
@@ -27,8 +30,20 @@ export class MemberService {
   }
 
   getMember(id: number): Observable<Member> {
-    this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
-    return of(MEMBERS.find(member => member.id === id)!); //配列の中でidがマッチするものを1つだけ取得
+    const url = `${this.membersUrl}/${id}`;
+    return this.http.get<Member>(url)
+      .pipe(
+        tap(_ => this.log(`社員データ(id=${id})を取得しました`)),
+        catchError(this.handleError<Member>(`getMember id=${id}`))
+      )
+  }
+
+  updateMember(member: Member): Observable<any> {
+    return this.http.put(this.membersUrl, member, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`社員データ(id=${member.id})を変更しました`)),
+        catchError(this.handleError<any>(`updateMember`))
+      )
   }
 
   private log(message: string) {
